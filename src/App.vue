6290f9195f97798205117c2c03aae6fa6e43df1a
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2021-08-30 16:32:13 -0300

    Projeto inicial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>{{ titulo }}</h2>
+    <div class="input-group">
+      <input type="text" v-model="novaTarefa" @keydown.enter="adicionarTarefa">
+      <span class="input-group-btn">
+        <button @click="adicionarTarefa" class="btn btn-primary">Adicionar Tarefa</button>
+      </span>
+    </div>
+    <ul>
+      <li v-for="(tarefa, index) in tarefas" :key="index" :class="{removed: tarefa.checked}">
+        <input type="checkbox" v-model="tarefa.checked">
+        <label> {{tarefa.titulo}} </label>
+      </li>
+    </ul>
+    <footer>
+      <em>Altere aqui o título da sua lista de tarefas</em>
+      <input type="text" v-model="titulo">
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      titulo: "Minha Lista de Tarefas",
+      novaTarefa: "",
+      tarefas: [
+        {titulo: "Estudar", checked: false},
+        {titulo: "Programar", checked: true}
+      ],
+    }
+  },
+  methods: {
+    adicionarTarefa() {
+      if(this.novaTarefa !== ""){
+        this.tarefas.push({
+        titulo: this.novaTarefa,
+        checked: false,
+      });
+        this.novaTarefa = "";
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+  width: 40%;
+  margin: 20px auto 0px auto;
 }
+
+ul li{
+  list-style: none;
+}
+
+.removed{
+  color:gray;
+}
+.removed label{
+  text-decoration: line-through;}
 </style>
